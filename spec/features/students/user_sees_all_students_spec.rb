86713fc94ff57dce6a8d1429b29d591aad918c03
apoc64 <sschwedt@gmail.com>
2018-05-15 14:45:46 -0600

    user can navigate to a student show from index

--- spec/features/students/user_sees_all_students_spec.rb
+++ spec/features/students/user_sees_all_students_spec.rb
@@ -25,9 +25,28 @@ describe 'user visits students index' do
     expect(page).to_not have_content(student1.name)
     expect(page).to have_content(student2.name)
   end
+
+  scenario 'a user can navigate to student show' do
+    student1 = Student.create!(name: 'Steve')
+    student2 = Student.create!(name: 'Steph')
+
+    visit students_path
+
+      click_link student1.name
+
+    expect(current_path).to eq(student_path(student1))
+    expect(page).to_not have_content(student2.name)
+    expect(page).to have_content(student1.name)
+  end
 end
-#   As a user
+
+# As a user
 # When I visit `/students`
-# And I click "Delete" next to a student's name
+# And I click on a student's name
+# I am taken to a show page for that student
+
+# #   As a user
+# # When I visit `/students`
+# # And I click "Delete" next to a student's name
 # I see the students index
 # And that student's name is no longer on the page
