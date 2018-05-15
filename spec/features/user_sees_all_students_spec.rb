88be94e219faf9177154aba4b83be073ee47c905
apoc64 <sschwedt@gmail.com>
2018-05-15 14:31:22 -0600

    user can delete students

--- spec/features/user_sees_all_students_spec.rb
+++ spec/features/user_sees_all_students_spec.rb
@@ -10,4 +10,24 @@ describe 'user visits students index' do
     expect(page).to have_content(student1.name)
     expect(page).to have_content(student2.name)
   end
+
+  scenario 'a user can delete a student' do
+    student1 = Student.create!(name: 'Steve')
+    student2 = Student.create!(name: 'Steph')
+
+    visit students_path
+
+    within(".student_#{student1.id}") do
+      click_link 'Delete'
+    end
+
+    expect(current_path).to eq(students_path)
+    expect(page).to_not have_content(student1.name)
+    expect(page).to have_content(student2.name)
+  end
 end
+#   As a user
+# When I visit `/students`
+# And I click "Delete" next to a student's name
+# I see the students index
+# And that student's name is no longer on the page
